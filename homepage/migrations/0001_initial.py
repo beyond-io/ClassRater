8d724b588749ffac7bf69c620740ad57b3be4325
ellaml <ella.mil1995@gmail.com>
2021-03-24 17:34:58 +0200

    Adding professor model and professor-to-course table

--- homepage/migrations/0001_initial.py
+++ homepage/migrations/0001_initial.py
@@ -45,4 +45,19 @@ class Migration(migrations.Migration):
                 ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='homepage.user')),
             ],
         ),
+        migrations.CreateModel(
+            name='Professor',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=100)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Professor_to_Course',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('course_id', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='homepage.course')),
+                ('professor_id', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='homepage.professor')),
+            ],
+        ),
     ]
