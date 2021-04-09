f680697843e988de0d5a02cdfeca8c84765bee4c
Ori Adler <ooriadler@gmail.com>
2021-04-09 13:58:44 +0300

    Add Review test data
    Uses `0005_review_test_data` to add 6 reviews to the database
    
    Assumptions:
      - User test data migration file contains at least 3 users
      - Course test data migration file contains at least 3 courses
    
    Notes:
      - This PR is dependent on PR #29 #35
      - This file tested against data added to the database
      via Django admin UI
    
    resolves: #36

---
+++ homepage/migrations/0005_review_test_data.py
@@ -0,0 +1,36 @@
+from django.db import migrations, transaction
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('homepage', '0001_initial'),
+        ('homepage', '0002_user_test_data'),
+        ('homepage', '0003_course_test_data'),
+    ]
+
+    def generate_data(apps, schema_editor):
+        from homepage.models import User
+        from homepage.models import Course
+        from homepage.models import Review
+        from django.utils import timezone
+
+        users = User.objects.all()
+        courses = Course.objects.all()
+
+        test_data_review = [
+            (1, courses[0].course_id, users[0].id, 5, timezone.now(), "Great course", 3),
+            (2, courses[0].course_id, users[0].id, 4, timezone.now(), "I've learned a lot!", 2),
+            (3, courses[1].course_id, users[1].id, 4.5, timezone.now(), "The course was difficult", 5),
+            (4, courses[1].course_id, users[1].id, 3.2, timezone.now(), "I didn't learn anything new", 3.7),
+            (5, courses[2].course_id, users[2].id, 4.5, timezone.now(), "This course helped me to find a job", 2.5),
+            (6, courses[2].course_id, users[2].id, 3.5, timezone.now(), "I didn't understand the material ", 4),
+        ]
+
+        with transaction.atomic():
+            for tdr in test_data_review:
+                Review(*tdr).save()
+
+    operations = [
+        migrations.RunPython(generate_data),
+    ]
