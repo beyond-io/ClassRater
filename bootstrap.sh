3a447429af0ede273591e7f4c290894779487504
Leon Rabinovich <leonra@mtac.ac.il>
2021-04-21 21:30:12 +0300

    Adding functions and tests for FollowedUserCourses model

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,7 @@ cd /vagrant
 pipenv sync --dev
 pipenv install Pillow
 pipenv run python manage.py migrate
+pipenv install --dev pytest
 
 # The app logs will be collected in nohup.out
 nohup pipenv run python manage.py runserver 0.0.0.0:8000 &
