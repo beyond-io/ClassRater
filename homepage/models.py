6c03897ec4a656794c9af52ef78bed9cc930748e
Leon Rabinovich <leonra@mta.ac.il>
2021-04-29 14:47:51 +0300

    Adding methods and tests to the AppUser model

--- homepage/models.py
+++ homepage/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.conf import settings
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import User
 
 
 class Course(models.Model):
@@ -71,10 +72,38 @@ class Prerequisites(models.Model):
 
 
 class AppUser(models.Model):
+    # This field is the built-in django user model
+    # We currently use the following attributes from the django user model: username, password, email, is_active
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
 
     def __str__(self):
         return self.user.username
+
+    def toggle_user_activation(self):
+        self.user.is_active = not(self.user.is_active)
+
+    @staticmethod
+    # Creates a new appUser (without superuser permmisions) and saves it in the DB
+    # The is_active attribute of AppUser.user is automaticly set to 'True' when creating a new AppUser
+    def create_appUser(username, email, password):
+        appUser = AppUser()
+        user = User.objects.create_user(username, email, password)
+        appUser.user = user
+
+        appUser.save()
+        return appUser
+
+    @staticmethod
+    def get_all_appUsers():
+        return list(AppUser.objects.all())
+
+    @staticmethod
+    def find_appUser(username_ToFind):
+        try:
+            user = User.objects.get(username=username_ToFind)
+            return AppUser.objects.get(user=user)
+        except User.DoesNotExist:
+            return None
 
 
 class FollowedUserCourses(models.Model):
