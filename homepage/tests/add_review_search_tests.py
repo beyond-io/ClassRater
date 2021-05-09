dde86e929ed259a8120c1489e69c974a26fdf9b4
Ori Adler <ooriadler@gmail.com>
2021-05-09 19:41:35 +0300

    remove redundant blank lines

--- homepage/tests/add_review_search_tests.py
+++ homepage/tests/add_review_search_tests.py
@@ -9,7 +9,6 @@ from pytest_django.asserts import assertTemplateUsed
 def test_type_get_courses_ordered_by_name():
     # because an empty string is a substring of all names, all courses returned
     courses = Course.get_courses_ordered_by_name("")
-
     assert isinstance(courses, QuerySet)
     assert all(isinstance(course, Course) for course in courses)
 
@@ -22,7 +21,6 @@ def test_type_get_courses_ordered_by_name():
 @pytest.mark.django_db
 def test_name_exists_get_courses_ordered_by_name(course_name_exist, expected_output):
     courses = Course.get_courses_ordered_by_name(course_name_exist)
-
     assert all(course.name in expected_output for course in courses)
 
 
@@ -33,14 +31,12 @@ def test_name_exists_get_courses_ordered_by_name(course_name_exist, expected_out
 @pytest.mark.django_db
 def test_wrong_name_get_courses_ordered_by_name(wrong_course_name):
     courses = Course.get_courses_ordered_by_name(wrong_course_name)
-
     assert not courses
 
 
 @pytest.mark.django_db
 def test_lexicography_order_get_courses_ordered_by_name():
     courses = list(Course.get_courses_ordered_by_name(""))
-
     for index in range(len(courses)-1):
         assert courses[index].name < courses[index+1].name
 
@@ -49,7 +45,6 @@ def test_lexicography_order_get_courses_ordered_by_name():
 @pytest.mark.django_db
 def test_renders_add_review_search_template(client):
     response = client.get('/add_review_search/')
-
     assert response.status_code == 200
     assertTemplateUsed(response, 'homepage/add_review_search.html')
 
@@ -58,7 +53,6 @@ def test_renders_add_review_search_template(client):
 def test_get_course_by_name_with_client(client):
     response = client.get('/add_review_search/', {'course': 'Resonance'})
     courses_not_found = [b'Grammatica in Arithmancy', b'Numerology', b'UnFogging the Future']
-
     assert response.status_code == 200
     assert b'Resonance' in response.content
     assert all(course not in response.content for course in courses_not_found)
@@ -67,5 +61,4 @@ def test_get_course_by_name_with_client(client):
 @pytest.mark.django_db
 def test_wrong_course_name_with_client(client):
     response = client.get('/add_review_search/', {'course': 'Introduction to Buddhism'})
-
     assert response.status_code == 200
